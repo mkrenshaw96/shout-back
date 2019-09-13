c80ae99d1f75149800c3dd7a68c64729a55221ae
Michael Crenshaw <mkrenshaw96@gmail.com>
2019-09-12 23:20:39 -0400

    Upadted npm packages as well as changed model validators to usernames as they were in the wrong spot on the model

--- Models/user.js
+++ Models/user.js
@@ -1,5 +1,5 @@
 module.exports = (Sequelize, DataTypes) => {
-	return Sequelize.define("user", {
+	return Sequelize.define('user', {
 		id: {
 			type: DataTypes.UUID,
 			defaultValue: DataTypes.UUIDV4,
@@ -7,22 +7,22 @@ module.exports = (Sequelize, DataTypes) => {
 		},
 		name: {
 			type: DataTypes.STRING,
-			allowNull: false,
-			validate: {
-				isAlphanumeric: {
-					args: true,
-					msg: "Username can only contain letters and numbers"
-				},
-				len: {
-					args: [3, 15],
-					msg: "Username must be 3-15 characters long."
-				}
-			}
+			allowNull: false
 		},
 		username: {
 			type: DataTypes.STRING,
 			allowNull: false,
-			unique: true
+			unique: true,
+			validate: {
+				isAlphanumeric: {
+					args: true,
+					msg: 'Username can only contain letters and numbers'
+				},
+				len: {
+					args: [3, 15],
+					msg: 'Username must be 3-15 characters long.'
+				}
+			}
 		},
 		password: {
 			type: DataTypes.STRING,
